f8a95d65af999efd0add59974769bb9c7fbb8f88
Y.Malchanau <yauhenmalchanau@gmail.com>
2023-02-05 18:35:07 +0300

    Init Spring Boot app

---
+++ src/main/java/com/travelblog/service/TravelBlogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.travelblog.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBlogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelBlogServiceApplication.class, args);
+	}
+
+}
